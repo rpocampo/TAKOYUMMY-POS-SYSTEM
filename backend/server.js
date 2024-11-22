0e1f27bfe2e64f2aa6a32a81a0dfc2f832e3accf
MRV-dev <a.marv0903@gmail.com>
2024-11-22 15:05:59 +0800

    konti lang

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
 require('dotenv').config()
 
+
 const express = require('express');
 const mongoose = require('mongoose');
 const account = require('./models/userModel'); // Path to your model file
 
 const app = express();
 
+const uri = 'mongodb+srv://Marv:Marv1003_@mern.ohwxw.mongodb.net/Login?retryWrites=true&w=majority';
+
 // Middleware
 app.use(express.json());
 
 // Database connection
-mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-
-    // Ensure the unique index for the account model is created
-    account.init().catch((error) => {
-        console.error('Error initializing account model:', error.message);
-    });
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        account.init().catch((error) => {
+            console.error('Error initializing account model:', error.message);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+    })
 
 // Routes
 const userRoutes = require('./routes/users'); // Example route file
